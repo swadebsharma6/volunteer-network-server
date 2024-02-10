be76d61f0312c3a72f011dcf5f22cbe45edc8287
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-10 21:42:08 +0600

    initail network server with express and mongodb

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fikwith.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+ 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Volunteer server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Volunteer server is running on port ${port}`)
+})
